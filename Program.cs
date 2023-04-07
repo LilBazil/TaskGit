68281a311a1d9754c1061e36890c7b4ecca40074
Lil Basil <bazikalova.alena@mail.ru>
2023-04-08 09:52:10 +1000

    test

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class MainClass
+{
+    public static void Main()
+    {
+        //int countOfVariations = 0;
+        string input = File.ReadAllText("input.txt").Trim();
+        List<string> rem = new();
+
+        while (input.Distinct().Count() > 1)
+        {
+            const int z = (int)'0';
+            const int n = (int)'9';
+            List<int> variation = new();
+
+            for (int i = z; i <= n; ++i)
+            {
+                string temp = "";
+                foreach (char a in input)    if (a != (char)i)    temp += a;
+                if (temp != input) variation.Add(Convert.ToInt32(temp));
+            }
+            int max = -1;
+            foreach (int var in variation)    if (var > max)    max = var;
+            rem.Add(Convert.ToString(max));
+            input = Convert.ToString(max);
+        }
+        string onOutput = "";
+        onOutput += rem.Count;
+        foreach (string str in rem)
+        {
+            onOutput += '\n';
+            onOutput += str;
+        }
+        File.WriteAllText("output.txt", onOutput);
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CATS1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("CATS1")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7ee69d9f-f937-40e7-a09c-d06d3a1b6ac3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
